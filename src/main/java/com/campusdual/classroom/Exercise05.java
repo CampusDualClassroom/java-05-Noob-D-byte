35dab6bf68e7694ca4880c18bf9195e33052a1ea
davidrey <david_sardy@hotmail.com>
2024-10-27 13:42:03 +0100

    Exercise05 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -8,7 +8,7 @@ public class Exercise05 {
         checkPositiveNegative(-100);
     }
 
-    //TODO ↓
+
     // Si el valor es menor, tiene que devolver el siguiente mensaje: X es menor que Y
     // Si el valor es mayor o igual, tiene que devolver el siguiente mensaje: X es mayor o igual que Y
     public static void checkLessOrGreaterEqual(int n1, int n2) {
@@ -19,7 +19,7 @@ public class Exercise05 {
         }
     }
 
-    //TODO ↓
+
     // Si es múltiplo, tiene que devolver el siguiente mensaje: X es múltiplo de Y
     // Si no es múltiplo, tiene que devolver el siguiente mensaje: X no es múltiplo de Y
     public static void checkMultiply(int n1, int n2) {
